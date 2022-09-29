785789d3ad9d7f15e38ef2ffdd45e9e4d0ae156a
Guillermo <guillearnautm@gmail.com>
2022-09-29 12:04:41 +0200

    A_nav_mesh_basics

---
+++ Assets/A_resources/PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+    Camera cam;
+
+	[SerializeField]
+    NavMeshAgent agent;
+
+	[SerializeField]
+	GameObject m_Pointer;
+
+	bool _spawned = false;
+	bool Spawned {
+		get { return _spawned; }
+		set { _spawned = _spawned || value; }
+	}
+
+    // Update is called once per frame
+    void Update () 
+    {
+
+		Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		bool collided = Physics.Raycast(ray, out hit);
+
+		if (collided)
+		{
+
+			if (!Spawned) {
+				m_Pointer = Instantiate(m_Pointer, hit.point, Quaternion.identity);
+				Spawned = true;
+			}
+
+				m_Pointer.SetActive(true);
+				m_Pointer.transform.position = hit.point;
+
+
+			if (Input.GetMouseButtonDown(0)) {
+				agent.SetDestination(hit.point);
+			}
+
+		} else {
+			m_Pointer.SetActive(false);
+		}
+
+    }
+}
